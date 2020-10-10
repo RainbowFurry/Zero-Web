4caff6b86f6a84eccd3615af8f53f26bf0c04ac0
DarkWolfCraft.net <darkwolfcraft.net@outlook.de>
2020-10-10 17:52:21 +0200

    Responsive Test3

--- Zero_Web/database/MongoDBManager.cs
+++ Zero_Web/database/MongoDBManager.cs
@@ -1,24 +1,27 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Driver;
 using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Web;
-using Zero_Web.Models;
 
 namespace Zero_Web.database
 {
     public class MongoDBManager
     {
-
+#if DEBUG
         private static protected readonly string user = ConfigurationManager.AppSettings["MongoDB_User"];
         private static protected readonly string password = ConfigurationManager.AppSettings["MongoDB_Password"];
         private static protected readonly string host = ConfigurationManager.AppSettings["MongoDB_Host"];
         private static protected readonly string port = ConfigurationManager.AppSettings["MongoDB_Port"];
         private static protected readonly string database = ConfigurationManager.AppSettings["MongoDB_Database"];
         private static protected readonly string connectionString = "mongodb://" + user + ":" + password + "@" + host + ":" + port + "/" + database;
-
+#else
+   private static protected readonly string user = ConfigurationManager.AppSettings["MongoDB_User_Debug"];
+        private static protected readonly string password = ConfigurationManager.AppSettings["MongoDB_Password_Debug"];
+        private static protected readonly string host = ConfigurationManager.AppSettings["MongoDB_Host_Debug"];
+        private static protected readonly string port = ConfigurationManager.AppSettings["MongoDB_Port_Debug"];
+        private static protected readonly string database = ConfigurationManager.AppSettings["MongoDB_Database_Debug"];
+        private static protected readonly string connectionString = "mongodb://" + user + ":" + password + "@" + host + ":" + port + "/" + database;
+#endif
         private static MongoClient client;
         private static IMongoDatabase db;
 
@@ -34,7 +37,7 @@ namespace Zero_Web.database
                 db = client.GetDatabase(database);
                 System.Diagnostics.Debug.WriteLine("MongoDB Connection created successfull!");
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 System.Diagnostics.Debug.WriteLine("MongoDB Connection created Error!");
             }
@@ -48,7 +51,7 @@ namespace Zero_Web.database
         /// <param name="result"></param>
         public void createEntry(string table, string searchWord, string result)
         {
-            
+
             try
             {
                 var things = db.GetCollection<BsonDocument>(table);
@@ -58,7 +61,7 @@ namespace Zero_Web.database
 
                 System.Diagnostics.Debug.WriteLine("MongoDB Entry created successfull!");
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 System.Diagnostics.Debug.WriteLine("MongoDB Entry created Error!");
             }
